1cb313954606314e1c053958799cc3df095dc09b
islam <cloplop091@gmail.com>
2023-10-09 02:04:08 +0300

    Test

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.template import Template, Context
+from django.template.exceptions import TemplateSyntaxError
 
-# Create your tests here.
+from app.models import MenuModel
+
+
+class MenuModelTestCase(TestCase):
+    def setUp(self):
+        self.menu1 = MenuModel.objects.create(name='Menu 1', named_url='menu-1')
+        self.menu2 = MenuModel.objects.create(name='Menu 2', named_url='menu-2', parent=self.menu1)
+        self.menu3 = MenuModel.objects.create(name='Menu 3', named_url='menu-3', parent=self.menu2)
+
+    def test_menu_model_str(self):
+        self.assertEqual(str(self.menu1), 'Menu 1')
+        self.assertEqual(str(self.menu2), 'Menu 2')
+        self.assertEqual(str(self.menu3), 'Menu 3')
+
+
+class MenuViewTestCase(TestCase):
+    def setUp(self):
+        self.menu1 = MenuModel.objects.create(name='Menu 1', named_url='menu-1')
+        self.menu2 = MenuModel.objects.create(name='Menu 2', named_url='menu-2', parent=self.menu1)
+        self.menu3 = MenuModel.objects.create(name='Menu 3', named_url='menu-3', parent=self.menu2)
+
+    def test_menu_list_view(self):
+        url = reverse('menu_list', args=[self.menu1.named_url])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<li>Menu 1')
+        self.assertContains(response, '<li>Menu 2')
+        self.assertContains(response, '<li>Menu 3')
+
--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:menu_name>/', views.menu_list)
+    path('<str:menu_name>/', views.menu_list, name='menu_list')
 ]
