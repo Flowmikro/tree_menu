9bd4476a811b1227ba45c15edbc4c41bee04764e
islam <cloplop091@gmail.com>
2023-10-08 18:50:13 +0300

    models & admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import MenuModel
+
+
+@admin.register(MenuModel)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('name', 'named_url', 'parent')
+    prepopulated_fields = {'named_url': ('name',)}
--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class MenuModel(models.Model):
+    """Древовидное меню"""
+    name = models.CharField('Название', max_length=150, help_text='Максимум 100 символов')
+    named_url = models.CharField('URL', max_length=150, null=True, blank=True)
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='children', help_text='Выберете пункт',
+                               verbose_name='Родитель', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Пункт меню'
+        verbose_name_plural = 'Пункты меню'
+
+    def __str__(self):
+        return self.name
