68ce0574398b8e21601f83044b9963e41194cb8b
islam <cloplop091@gmail.com>
2023-10-08 19:38:52 +0300

    urls & view & html

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from app.models import MenuModel
+
+
+def menu_list(request, menu_name):
+    menu = get_object_or_404(MenuModel, named_url=menu_name)
+    context = {'menu': menu}
+    return render(request, 'app/menu.html', context)
