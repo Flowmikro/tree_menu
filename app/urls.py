505ba358524b68a8e9d81ab51b0f09c0c0a07604
islam <cloplop091@gmail.com>
2023-10-09 03:14:26 +0300

    Update

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:menu_name>/', views.menu_list, name='menu_list')
+    path('<str:menu_name>/', views.menu_list, name='menu_list'),  # http://127.0.0.1:8000/menyu/
 ]
